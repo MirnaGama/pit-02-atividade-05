31b8eac912b8a5c87cea32aeeb756d61da580033
romulolyra <65560137+romulolyra@users.noreply.github.com>
2021-02-19 16:27:23 -0300

    primeira

--- frontend-cra/src/pages/TodoDetails/index.jsx
+++ frontend-cra/src/pages/TodoDetails/index.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 import axios from '../../utils/api';
-
 import Card from '../../components/Card';
+import Todo from '../../components/Todo';
 
 export default function index() {
-    const todo = useState([]);
+  const todo = useState([]);
 
-    const findTodo = async (id) => {
-        const response; // TO-DO: Usar a API para buscar o objeto pelo ID
-        
-        // setTodo(response.data);
-        // TO-DO: Colocar a response num objeto ToDo e mostrar as informações na tela
-      };
-    
-      useEffect(() => {
-        findTodo(/*parametro ID aqui*/); // TO-DO: Buscar o parâmetro do ID no componente através do Effect
-      }, []);
+  const findTodo = async (id) => {
+    // const response; // TO-DO: Usar a API para buscar o objeto pelo ID
+    const response = await axios.get(`/todo/${todo.id}`, {
+      ...todo,
+
+    });
+    // setTodo(response.data);
+  };
+
+  useEffect(() => {
+    const oId = useParams;
+    findTodo(oId);
+    // TO-DO: Buscar o parâmetro do ID no componente através do Effect
+  }, []);
   return (
     <Container>
       <Card title="Todo App" className="m-4">
